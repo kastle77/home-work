a1c9145e72b1bb8124e9d13d4f84d2b3cf291075
kastle77 <kastlegod@gmail.com>
2024-11-05 17:11:51 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+index = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while index < len(my_list):
+    if my_list[index] < 0 :
+        break
+    if my_list[index] > 0 :
+        print(my_list[index])
+    index += 1
