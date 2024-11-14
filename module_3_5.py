eed075e9395839b98a0492d0ca6087656d86ceac
kastle77 <kastlegod@gmail.com>
2024-11-14 20:34:39 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,15 @@
+from gc import get_referents
+
+
 def get_multiplied_digits(number):
     str_number = str(number)
     if len(str_number) > 1:
         first = int(str_number[0])
+        if int(str_number[-1]) == 0:
+            return get_multiplied_digits(str_number[0:-1])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
-
 result = get_multiplied_digits(40203)
+
 print(result)
