1d1536313aab219c726624377df2a0447272f12d
kastle77 <kastlegod@gmail.com>
2025-01-14 11:54:37 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from true_math import divide as TM_d
+from fake_math import divide as FM_d
+
+
+result1 = FM_d (69, 3)
+result2 = FM_d(3, 0)
+result3 = TM_d(49, 7)
+result4 = TM_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
